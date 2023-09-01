139d082fa434acbd07c4ab920daa91f4477467d0
Gourav soni <gouravsoni8476@gmail.com>
2023-09-01 12:01:41 +0530

    created a project from 30DOJs called KeyPressed, which shows the keyCode and name of Key from the keyboard

---
+++ keyPressed.js
@@ -0,0 +1,13 @@
+let container = document.querySelector(".container");
+let title = document.querySelector(".title");
+let p = document.createElement("p");
+let span = document.createElement("span");
+document.addEventListener("keydown", (e) => {
+  span.textContent = e.key;
+  span.setAttribute('class', 'span')
+  title.textContent = `You have pressed `;
+  title.append(span);
+  p.textContent = e.keyCode;
+  p.setAttribute("class", "keyValue");
+  container.appendChild(p);
+});
